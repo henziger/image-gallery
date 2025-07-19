5550896cffde5f3754c325bc8ece442e7dc8bf95
Eric Henziger <5124091+henziger@users.noreply.github.com>
2025-07-19 12:39:34 +0200

    Working on the link preview wording...

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,13 +14,13 @@ class MyDocument extends Document {
           <meta property="og:title" content="Eric Henziger's image archive" />
           <meta
             property="og:description"
-            content="A cool image from Eric, check it out!"
+            content="Here's a link to one or more of Eric's favorite photos, check it out!"
           />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Eric Henziger's image archive" />
           <meta
             name="twitter:description"
-            content="A cool image from Eric, check it out!"
+            content="Here's a link to one or more of Eric's favorite photos, check it out!"
           />
         </Head>
         <body className="bg-black antialiased">
