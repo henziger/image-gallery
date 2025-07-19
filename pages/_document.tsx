534258a785937ab1902a0fa9c599961c590e4731
Eric Henziger <5124091+henziger@users.noreply.github.com>
2025-07-19 12:35:12 +0200

    Attempt to make link previews less repetitive

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,16 +11,16 @@ class MyDocument extends Document {
             content="Eric Henziger's image archive"
           />
           <meta property="og:site_name" content="henziger.se" />
+          <meta property="og:title" content="Eric Henziger's image archive" />
           <meta
             property="og:description"
-            content="Eric Henziger's image archive"
+            content="A cool image from Eric, check it out!"
           />
-          <meta property="og:title" content="Eric Henziger's image archive" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Eric Henziger's image archive" />
           <meta
             name="twitter:description"
-            content="Eric Henziger's image archive"
+            content="A cool image from Eric, check it out!"
           />
         </Head>
         <body className="bg-black antialiased">
